2f2b47796b762d14f89c28b51b7865ea5c7e9c7a
DESKTOP-1LH23A7\268ga <rap.97268@gmail.com>
2023-03-08 22:50:10 +0900

    コメント位置に違和感がある為変更

--- Radiotalk_downloads.py
+++ Radiotalk_downloads.py
@@ -14,13 +14,12 @@ drive.mount('/content/drive')
 response = requests.get(rss_url)
 urls = re.findall('https://.*\.m4a', response.text)
 
-# ③ ②のURLにアクセスする。
 download_dir = '/content/drive/MyDrive/Downloads/'
 if not os.path.exists(download_dir):  # ダウンロード先のフォルダが存在しない場合は作成する
     os.makedirs(download_dir)
 
+# ③ m4aファイルをダウンロードする。
 for url in urls:
-    # ④ m4aファイルをダウンロードする。
     title = ""
     root = ET.fromstring(requests.get(rss_url).text)
     for item in root.findall("./channel/item"):
@@ -28,10 +27,10 @@ for url in urls:
             title = item.find("title").text
             break
     file_name = title + ".m4a"
-    path = os.path.join(download_dir, file_name)  # ダウンロード先をGoogle Drive内に変更する
+    path = os.path.join(download_dir, file_name)  
     response = requests.get(url)
     with open(path, "wb") as f:
         f.write(response.content)
     
-# ⑤ 全てダウンロード完了した際に、完了メッセージを表示
+# ④ 全てダウンロード完了した際に、完了メッセージを表示
 print("ダウンロードが完了しました。")
